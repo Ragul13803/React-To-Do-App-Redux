321bb676f99b684afc94f95eab7dc8afde972dea
Ragul13803 <rglsrsh@gmail.com>
2025-05-27 18:52:41 +0530

    build error fix

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Login from './components/Auth/login'
 import Layout from './components/layout'
